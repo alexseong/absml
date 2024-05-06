8c6169c6e0cd5bb99c833c0306f63b6fa47ae33d
Alex Seong <alex.y.seong@gmail.com>
2024-05-06 16:36:04 -0400

    initial commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read.()
+
+setuptools.setup(
+    name = "absml",
+    version = "0.0.1",
+    author = "Alex Seong",
+    author_email = "alex.y.seong@gmail.com",
+    description = "machine learning utilities for manufacturing data processing",
+    long_description = long_description,
+    url = 
+)
